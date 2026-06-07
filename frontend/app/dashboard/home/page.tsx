7af1602dede6af59b239fc4c7ddd491f65b6d0df
Keerthanreddy01 <keerthan101011@gmail.com>
2026-06-07 22:42:02 +0530

    fix: change desktop composer from fixed floating bar to static inline card

--- frontend/app/dashboard/home/page.tsx
+++ frontend/app/dashboard/home/page.tsx
@@ -199,7 +199,7 @@ function BottomComposerBar({ user, onPostCreated }: { user: any; onPostCreated: 
   };
 
   return (
-    <div className="hidden md:flex fixed bottom-16 md:bottom-6 left-1/2 -translate-x-1/2 z-40 flex-col items-center justify-end pointer-events-none w-full max-w-[600px] px-4">
+    <div className="hidden md:flex flex-col w-full mb-6 relative z-40">
       <motion.div
         ref={containerRef}
         layout
@@ -689,6 +689,10 @@ export default function DashboardHomePage() {
       {/* Main Feed Area */}
       <main className="flex-1 flex justify-center h-full overflow-y-auto overflow-x-hidden no-scrollbar relative z-10 md:pl-[72px] xl:pr-[340px] w-full max-w-full">
         <div className="w-full max-w-full md:max-w-[680px] flex flex-col pt-8 pb-20 md:pb-24 mx-auto px-4 overflow-x-hidden">
+          
+          {/* Desktop Composer (Hidden on mobile) */}
+          <BottomComposerBar user={user} onPostCreated={() => { }} />
+
           {/* Stories Reel Mock */}
           <div className="flex gap-4 overflow-x-auto no-scrollbar mb-8 px-2 sm:px-0">
             {stories.map((story, i) => (
@@ -722,9 +726,6 @@ export default function DashboardHomePage() {
       </main>
 
       <RightSidebar />
-
-      {/* Fixed bottom composer bar + slide-up drawer */}
-      <BottomComposerBar user={user} onPostCreated={() => { }} />
     </div>
   );
 }
